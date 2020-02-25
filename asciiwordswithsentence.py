9a2be02cc5cf6b46b604a14d214299401f1c2474
3007Venki <61415188+3007Venki@users.noreply.github.com>
2020-02-25 17:53:09 +0530

    Add files via upload

---
+++ asciiwordswithsentence.py
@@ -0,0 +1,19 @@
+thisdict={
+        "I":["F",ord("F")],
+        "thank":["G",ord("G")],
+	"you":["H",ord("H")],
+	"for":["I",ord("I")],
+	"your":["J",ord("J")],
+	"review":["K",ord("K")],
+	"of":["L",ord("L")],
+	"my":["M",ord("M")],
+	"first":["N",ord("N")],
+	"python":["O",ord("O")],
+	"code":["P",ord("P")]}
+for x, y in thisdict.items():
+    print(x,"-",y[0],"-",y[1])
+    
+    
+    
+
+    
